fc46ef469e8a0088dfe1cf16205b1f2d8f5eb4c1
Li Mengyu <aumengyu@gmail.com>
2024-07-02 14:40:52 +0930

    update

--- src/components/OrderConfirmation.js
+++ src/components/OrderConfirmation.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const OrderConfirmation = () => {
-
   return (
     <div className="container mt-5 text-center">
       <h1 className="mb-4">订单确认</h1>
+      <div className="mb-4" style={{ fontSize: '50px' }}>🎉</div>
       <p className="lead">恭喜你，下单成功！</p>
       <p>您的订单已成功提交，我们会尽快处理并发货。</p>
     </div>
