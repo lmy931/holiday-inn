e17daa4f15346161d676d0d484d87c11f72f6a92
Li Mengyu <aumengyu@gmail.com>
2024-07-03 19:01:08 +0930

    update

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ import usePreloadImages from './components/usePreloadImages';
 const App = () => {
   const provinces = [
     'anhui.png', 'beijing.png', 'chongqing.png', 'fujian.png', 'gansu.png', 
-    'guangdong.png', 'hubei.png', 'hunan.png', 'jiangsu.png', 'liaoning.png',
-    'shandong.png', 'shanxi.png', 'sichuan.png', 'tianjin.png', 'zhejiang.png'
+    'guangdong.png', 'guangxi.png', 'guizhou.png', 'hainan.png', 'hebei.png',
+    'heilongjiang.png', 'henan.png', 'hubei.png', 'hunan.png', 'jiangsu.png',
+    'jiangxi.png', 'jilin.png', 'liaoning.png', 'neimenggu.png', 'ningxia.png',
+    'qinghai.png', 'shandong.png', 'shanxi.png', 'shanxi3.png', 'shanghai.png',
+    'sichuan.png', 'tianjin.png', 'xinjiang.png', 'yunnan.png', 'zhejiang.png'
   ];
   const imageUrls = provinces.map(province => `${process.env.REACT_APP_BASE_URL}/images/provinces/${province}`);
   usePreloadImages(imageUrls);
@@ -28,4 +31,4 @@ const App = () => {
   );
 }
 
-export default App; // Ensure that you use `export default` here
+export default App;
