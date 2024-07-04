ee1ede16041dd6168dcf0b83592550289984f373
Li Mengyu <aumengyu@gmail.com>
2024-07-04 23:06:55 +0930

    update

--- src/components/ProvinceSelector.js
+++ src/components/ProvinceSelector.js
@@ -1,62 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/ProvinceSelector.css'; // 引入自定义CSS文件
+import { ChinaMapPaths } from '../data/ChinaMapPaths'; // 引入SVG内容
 
 const ProvinceSelector = () => {
   const navigate = useNavigate();
-  const provinces = [
-    { name: '安徽', img: 'anhui.png' },
-    { name: '北京', img: 'beijing.png' },
-    { name: '重庆', img: 'chongqing.png' },
-    { name: '福建', img: 'fujian.png' },
-    { name: '甘肃', img: 'gansu.png' },
-    { name: '广东', img: 'guangdong.png' },
-    { name: '广西', img: 'guangxi.png' },
-    { name: '贵州', img: 'guizhou.png' },
-    { name: '海南', img: 'hainan.png' },
-    { name: '河北', img: 'hebei.png' },
-    { name: '黑龙江', img: 'heilongjiang.png' },
-    { name: '河南', img: 'henan.png' },
-    { name: '湖北', img: 'hubei.png' },
-    { name: '湖南', img: 'hunan.png' },
-    { name: '江苏', img: 'jiangsu.png' },
-    { name: '江西', img: 'jiangxi.png' },
-    { name: '吉林', img: 'jilin.png' },
-    { name: '辽宁', img: 'liaoning.png' },
-    { name: '内蒙古', img: 'neimenggu.png' },
-    { name: '宁夏', img: 'ningxia.png' },
-    { name: '青海', img: 'qinghai.png' },
-    { name: '山东', img: 'shandong.png' },
-    { name: '山西', img: 'shanxi.png' },
-    { name: '陕西', img: 'shanxi3.png' },
-    { name: '上海', img: 'shanghai.png' },
-    { name: '四川', img: 'sichuan.png' },
-    { name: '天津', img: 'tianjin.png' },
-    { name: '新疆', img: 'xinjiang.png' },
-    { name: '云南', img: 'yunnan.png' },
-    { name: '浙江', img: 'zhejiang.png' }
-  ];
 
-  const handleProvinceClick = (provinceName) => {
-    navigate(`/products?province=${provinceName}`);
+  const handleProvinceClick = (event) => {
+    let provinceName = event.target.getAttribute('name');
+    if (provinceName) {
+      // 转换省份名称为小写并去掉下划线
+      provinceName = provinceName.replace(/_/g, '').toLowerCase();
+      console.log(provinceName);
+      navigate(`/products?province=${provinceName}`);
+    }
   };
 
   return (
     <div className="container mt-5">
       <h1 className="text-center mb-4">请选择您的省份</h1>
-      <ul className="list-group">
-        {provinces.map(province => (
-          <li
-            key={province.name}
-            onClick={() => handleProvinceClick(province.name)}
-            className="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
-            style={{ cursor: 'pointer' }}
-          >
-            {province.name}
-            <span className="badge badge-primary badge-pill">&gt;</span>
-          </li>
-        ))}
-      </ul>
+      <div
+        className="china-map"
+        onClick={handleProvinceClick}
+        dangerouslySetInnerHTML={{ __html: ChinaMapPaths }}
+      />
     </div>
   );
 }
